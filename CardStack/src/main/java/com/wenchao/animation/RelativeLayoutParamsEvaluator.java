31f3ba116ad2019eab072065822b6fda9b09c8e8
wenchao <wenchao@wenchaos-MacBook-Pro.local>
2015-12-18 14:48:54 +0000

    remove copy contructor in animation evaluator

--- CardStack/src/main/java/com/wenchao/animation/RelativeLayoutParamsEvaluator.java
+++ CardStack/src/main/java/com/wenchao/animation/RelativeLayoutParamsEvaluator.java
@@ -4,13 +4,15 @@ import android.animation.TypeEvaluator;
 import android.widget.RelativeLayout.LayoutParams
         ;
 
+import com.wenchao.cardstack.CardUtils;
+
 public class RelativeLayoutParamsEvaluator implements TypeEvaluator<LayoutParams> {
 
     @Override
     public LayoutParams evaluate(float fraction, LayoutParams start,
                                  LayoutParams end) {
 
-        LayoutParams result = new LayoutParams(start);
+        LayoutParams result = CardUtils.cloneParams(start);
         result.leftMargin += ((end.leftMargin-start.leftMargin)*fraction);
         result.rightMargin += ((end.rightMargin-start.rightMargin)*fraction);
         result.topMargin += ((end.topMargin-start.topMargin)*fraction);
