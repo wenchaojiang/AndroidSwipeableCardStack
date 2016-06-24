3f297e9f1f7238599ad993327d8f936dfe1ca8ac
Juan Pablo Prado <juan@juanpabloprado.com>
2016-06-24 16:27:55 -0500

    Be aware of issue with onClick events from views inside card content

--- DemoApp/src/main/java/wxj/swipeablecardstack/CardsDataAdapter.java
+++ DemoApp/src/main/java/wxj/swipeablecardstack/CardsDataAdapter.java
@@ -7,17 +7,27 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CardsDataAdapter extends ArrayAdapter<String> {
 
+    private Context context;
+
     public CardsDataAdapter(Context context) {
         super(context, R.layout.card_content);
+        this.context = context;
     }
 
     @Override
     public View getView(int position, final View contentView, ViewGroup parent){
         TextView v = (TextView)(contentView.findViewById(R.id.content));
         v.setText(getItem(position));
+        ImageView imageView = (ImageView) contentView.findViewById(R.id.image_view);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Toast.makeText(context, "Image clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
         return contentView;
     }
 
