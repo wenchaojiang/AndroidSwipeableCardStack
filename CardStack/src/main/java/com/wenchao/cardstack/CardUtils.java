4f63cd1aed839ff53f4afb6076a74f113d583e40
RAE <798404374@qq.com>
2016-07-06 18:30:07 +0800

    - 添加循环滚动 - 添加旋转配置 - 添加卡片方向配置

--- CardStack/src/main/java/com/wenchao/cardstack/CardUtils.java
+++ CardStack/src/main/java/com/wenchao/cardstack/CardUtils.java
@@ -11,19 +11,24 @@ public class CardUtils {
     final static int DIRECTION_BOTTOM_LEFT = 2;
     final static int DIRECTION_BOTTOM_RIGHT = 3;
 
-    public static void scale(View v, int pixel){
-        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)v.getLayoutParams();
-        params.leftMargin -= pixel;
-        params.rightMargin -= pixel;
-        params.topMargin -= pixel;
+    public static void scale(View v, int pixel, int gravity) {
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) v.getLayoutParams();
+        params.leftMargin -= pixel * 3;
+        params.rightMargin -= pixel * 3;
+        if (gravity == CardAnimator.TOP) {
+            params.topMargin += pixel;
+        } else {
+            params.topMargin -= pixel;
+        }
         params.bottomMargin -= pixel;
         v.setLayoutParams(params);
     }
 
-    public static LayoutParams getMoveParams(View v, int upDown,int leftRight){
-        RelativeLayout.LayoutParams original = (RelativeLayout.LayoutParams)v.getLayoutParams();
+    public static LayoutParams getMoveParams(View v, int upDown, int leftRight) {
+        RelativeLayout.LayoutParams original = (RelativeLayout.LayoutParams) v.getLayoutParams();
         //RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(original);
         RelativeLayout.LayoutParams params = cloneParams(original);
+
         params.leftMargin += leftRight;
         params.rightMargin -= leftRight;
         params.topMargin -= upDown;
@@ -31,45 +36,54 @@ public class CardUtils {
         return params;
     }
 
-    public static void move(View v, int upDown,int leftRight){
-        RelativeLayout.LayoutParams params = getMoveParams(v,upDown,leftRight);
+    public static void move(View v, int upDown, int leftRight) {
+        RelativeLayout.LayoutParams params = getMoveParams(v, upDown, leftRight);
         v.setLayoutParams(params);
     }
 
-    public static LayoutParams scaleFrom(View v, LayoutParams params, int pixel) {
+    public static LayoutParams scaleFrom(View v, LayoutParams params, int pixel, int gravity) {
         Log.d("pixel", "onScroll: " + pixel);
         params = cloneParams(params);
         params.leftMargin -= pixel;
         params.rightMargin -= pixel;
-        params.topMargin -= pixel;
+        if (gravity == CardAnimator.TOP) {
+            params.topMargin += pixel;
+        } else {
+            params.topMargin -= pixel;
+        }
         params.bottomMargin -= pixel;
-        Log.d("pixel", "onScroll: " + pixel);
         v.setLayoutParams(params);
 
         return params;
     }
 
-    public static LayoutParams moveFrom(View v, LayoutParams params, int leftRight, int upDown) {
+    public static LayoutParams moveFrom(View v, LayoutParams params, int leftRight, int upDown, int gravity) {
         params = cloneParams(params);
         params.leftMargin += leftRight;
         params.rightMargin -= leftRight;
-        params.topMargin -= upDown;
-        params.bottomMargin += upDown;
+
+        if (gravity == CardAnimator.BOTTOM) {
+            params.bottomMargin += upDown;
+            params.topMargin -= upDown;
+        } else {
+            params.bottomMargin -= upDown;
+            params.topMargin += upDown;
+        }
         v.setLayoutParams(params);
 
         return params;
     }
 
     //a copy method for RelativeLayout.LayoutParams for backward compartibility
-    public static RelativeLayout.LayoutParams cloneParams(RelativeLayout.LayoutParams params){
-        RelativeLayout.LayoutParams copy = new RelativeLayout.LayoutParams(params.width,params.height);
+    public static RelativeLayout.LayoutParams cloneParams(RelativeLayout.LayoutParams params) {
+        RelativeLayout.LayoutParams copy = new RelativeLayout.LayoutParams(params.width, params.height);
         copy.leftMargin = params.leftMargin;
         copy.topMargin = params.topMargin;
         copy.rightMargin = params.rightMargin;
         copy.bottomMargin = params.bottomMargin;
         int[] rules = params.getRules();
-        for (int i = 0 ; i< rules.length; i++ ){
-            copy.addRule(i,rules[i]);
+        for (int i = 0; i < rules.length; i++) {
+            copy.addRule(i, rules[i]);
         }
         //copy.setMarginStart(params.getMarginStart());
         //copy.setMarginEnd(params.getMarginEnd());
@@ -79,20 +93,20 @@ public class CardUtils {
 
     public static float distance(float x1, float y1, float x2, float y2) {
 
-        return (float) Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+        return (float) Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
     }
 
     public static int direction(float x1, float y1, float x2, float y2) {
-        if(x2>x1){//RIGHT
-            if(y2>y1){//BOTTOM
+        if (x2 > x1) {//RIGHT
+            if (y2 > y1) {//BOTTOM
                 return DIRECTION_BOTTOM_RIGHT;
-            }else{//TOP
+            } else {//TOP
                 return DIRECTION_TOP_RIGHT;
             }
-        }else{//LEFT
-            if(y2>y1){//BOTTOM
+        } else {//LEFT
+            if (y2 > y1) {//BOTTOM
                 return DIRECTION_BOTTOM_LEFT;
-            }else{//TOP
+            } else {//TOP
                 return DIRECTION_TOP_LEFT;
             }
         }
